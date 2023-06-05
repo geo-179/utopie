752cccc77810e01e241ae22016f40fdd01a503d0
William Foulks <wjfoulks@gmail.com>
2023-06-05 15:37:46 +0800

    Added index and create methods in comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(param[:post_id])
+    @comment.post = @post
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
