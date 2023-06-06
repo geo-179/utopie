47e9e807a249f1598adb20ad40f37431bd5966d4
Gio Redacted <gio.huh.179@gmail.com>
2023-06-06 12:25:50 +0800

    Chatting websocket complete and tested

---
+++ app/javascript/controllers/post_subscription_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="post-subscription"
+export default class extends Controller {
+  static values = { postId: Number }
+  static targets = ["comments"]
+
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "PostChannel", id: this.postIdValue },
+      { received: data => this.#insertCommentAndScrollDown(data) }
+    )
+    console.log(`Subscribed to the Post with the id ${this.postIdValue}.`)
+  }
+
+  #insertCommentAndScrollDown(data) {
+    this.commentsTarget.insertAdjacentHTML("beforeend", data)
+    this.commentsTarget.scrollTo(0, this.commentsTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+}
