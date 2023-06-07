786e72bd1c589409323b1aec6f5e57cacc30d042
William Foulks <wjfoulks@gmail.com>
2023-06-07 11:30:34 +0800

    Added category to search bar form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,10 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = policy_scope(Post)
-    # if params[:category].present?
-    #     @posts = @posts.where(category: params[:category])
-    # end
+
+    if params[:category].present?
+      @posts = @posts.where(category: params[:category])
+    end
 
     if params[:keyword].present?
       @posts = @posts.where("title ILIKE ?", "%#{params[:keyword]}%")
