9340756dea2c801b88eebc7dfa28b9fcb0109b08
Cody Young <youngcodz124@gmail.com>
2023-06-14 16:36:27 +0800

    fixing bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://www.utopie.site" }
-  config.action controller.asset host = "http://www.utopie.site"
+  # config.action controller.asset host = "http://www.utopie.site"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
